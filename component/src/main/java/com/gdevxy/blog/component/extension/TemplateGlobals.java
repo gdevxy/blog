e9414892e4dee713980c977d5b3fda7968970d23
Gabriel Dinant <gdevxy@gmail.com>
2025-01-02 10:21:35 +0100

    Added RSS feed button

--- component/src/main/java/com/gdevxy/blog/component/extension/TemplateGlobals.java
+++ component/src/main/java/com/gdevxy/blog/component/extension/TemplateGlobals.java
@@ -2,10 +2,13 @@ package com.gdevxy.blog.component.extension;
 
 import io.quarkus.qute.TemplateGlobal;
 import io.quarkus.runtime.configuration.ConfigUtils;
+import org.eclipse.microprofile.config.ConfigProvider;
 
 @TemplateGlobal
 public class TemplateGlobals {
 
 	public static final Boolean runningInProduction = ConfigUtils.isProfileActive("prod");
+
+	public static final String baseUri = ConfigProvider.getConfig().getValue("application.base-uri", String.class);
 
 }
