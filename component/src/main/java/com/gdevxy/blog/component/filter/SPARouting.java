c44f2541efb0d531566dfebfd551ca2472d460dd
Gabriel Dinant <gdevxy@gmail.com>
2025-11-29 19:45:39 +0100

    Fixed SPA

---
+++ component/src/main/java/com/gdevxy/blog/component/filter/SPARouting.java
@@ -0,0 +1,31 @@
+package com.gdevxy.blog.component.filter;
+
+import io.vertx.ext.web.Router;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Observes;
+
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+// as per https://docs.quarkiverse.io/quarkus-quinoa/dev/advanced-guides.html#spa-routing
+@ApplicationScoped
+public class SPARouting {
+
+	private static final String[] PATH_PREFIXES = {"/q/", "/api/", "/@"};
+	private static final Predicate<String> FILE_NAME_PREDICATE = Pattern.compile(".+\\.[a-zA-Z0-9]+$")
+			.asMatchPredicate();
+
+	public void init(@Observes Router router) {
+		router.get("/*").handler(rc -> {
+			final String path = rc.normalizedPath();
+			if (!path.equals("/")
+					&& Stream.of(PATH_PREFIXES).noneMatch(path::startsWith)
+					&& !FILE_NAME_PREDICATE.test(path)) {
+				rc.reroute("/");
+			} else {
+				rc.next();
+			}
+		});
+	}
+}
